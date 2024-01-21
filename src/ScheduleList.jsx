6bd3a95789282ad5f0dc57112ee33b01fcd311b6
Ayuto <Ayuto1215@outlook.com>
2024-01-21 18:15:01 +0900

    init

---
+++ src/ScheduleList.jsx
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import Modal from './Modal'; // モーダルコンポーネントのインポート
+import ClassDetailsModal from './ClassDetailsModal'; // モーダルコンポーネントのインポート
+import './TimeTable.css';
+import './Modal.css';
+import './ClassDetailsModal.css';
+import Container from '@mui/material/Container';
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Card from '@mui/material/Card';
+import Carousel from 'react-material-ui-carousel';
+import ArrowBackIosSharpIcon from '@mui/icons-material/ArrowBackIosSharp';//左矢印アイコン
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';//右矢印アイコン
+
+
+const ScheduleList = () => {
+  const [schedules, setSchedules] = useState([]);
+  const location = useLocation();
+
+  // URLからクエリパラメータを解析する関数
+  const useQuery = () => {
+    return new URLSearchParams(location.search);
+  };
+
+  const query = useQuery();
+  const department = query.get('department'); // URLの 'department' クエリパラメータを取得
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://ishiike.herokuapp.com/timetable/schedules/?department=${department}`);
+        setSchedules(response.data);
+      } catch (error) {
+        console.error('データの取得に失敗しました', error);
+      }
+    };
+
+    if (department) {
+      fetchData();
+    }
+  }, [department]);
+  const [selectedClass, setSelectedClass] = useState(null);
+  const days = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日'];
+  const timeSlots = {
+    '1時間目': '08:50~ 10:20',
+    '2時間目': '10:30~ 12:00',
+    '3時間目': '13:00~ 14:30',
+    '4時間目': '14:40~ 16:10',
+    '5時間目': '16:20~ 17:50',
+    '6時間目': '18:00~ 19:30'
+  };
+  const handleCellClick = async (day, period, classInfo) => {
+    if (classInfo) {
+      // 授業の詳細情報を表示するロジック
+      showClassDetails(classInfo);
+    }
+  };
+  const showClassDetails = (classInfo) => {
+    setSelectedClass(classInfo);
+  };
+  const renderSchedule = (classes, day, period) => {
+    if (!Array.isArray(schedules)) {
+      return '';
+    }
+  
+    const classInfo = classes.find(c => c.day === day && c.period === period);
+    if (!classInfo) {
+      return '';
+    }
+  
+    const handleClassInfoClick = (e) => {
+      e.stopPropagation();
+      handleCellClick(day, period, classInfo);
+    };
+    return (
+      <div onClick={handleClassInfoClick}>
+        <div className="class-name-timetable">{classInfo.name}</div>
+        <div className="class-room">{classInfo.building} {classInfo.room_id}</div>
+      </div>
+    );
+  };
+  const closeClassDetails = () => {
+    setSelectedClass(null);
+  };
+  const [modalShow, setModalShow] = useState(false);
+  const [currentClasses, setCurrentClasses] = useState([]);
+  const handleClassSelect = async (classInfo) => {
+    // モーダルを閉じる
+    setModalShow(false);
+  };
+
+  return (
+    <Container>
+      <Typography variant='h4' component='h1' my={2}>時間割一覧</Typography>
+      <Carousel
+      navButtonsAlwaysVisible = {true}
+      stopAutoPlayOnHover = {false}
+      swipe = {true}
+      interval = {10000000000000000} 
+      >
+      {schedules.map(schedule => (
+        <Box key={schedule.id} className="TimeTable">
+          <Typography component='h2'>{schedule.department}の先輩の時間割</Typography>
+          {/* 他のスケジュール情報も表示 */}
+            <ClassDetailsModal
+                classInfo={selectedClass}
+                onClose={closeClassDetails}
+            />
+            <Modal
+                show={modalShow}
+                classes={currentClasses}
+                onSelect={handleClassSelect}
+                onClose={() => setModalShow(false)}
+            />
+          <table>
+            <thead>
+            <tr>
+                <th>時間\曜日</th>
+                {days.map(day => <th key={day}>{day}</th>)}
+            </tr>
+            </thead>
+            <tbody>
+            {Object.entries(timeSlots).map(([period, time]) => (
+                <tr key={period}>
+                    <td>{`${period} ${time}`}</td>
+                {days.map(day => (
+                    <td key={day} onClick={() => handleCellClick(day, period)}>
+                    {renderSchedule(schedule.classes, day, period)}
+                    </td>
+                ))}
+                </tr>
+            ))}
+            </tbody>
+          </table>
+        </Box>
+      ))}
+      </Carousel>
+    </Container>
+  );
+};
+
+export default ScheduleList;
